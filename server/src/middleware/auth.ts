4f377795ad5e31e1dec1978592c108d1bc8a0112
mbaker <mekenna.drabner@gmail.com>
2024-09-18 15:23:05 -0600

    created functions for auth.ts middleware, server side.

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,22 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers.authorization; // get auth header
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1]; // get token from header
+
+    const secretKey = process.env.JWT_SECRET_KE || ''; // get secret key from env
+
+     jwt.verify(token, secretKey, (err, user) => { // verify token
+      if (err) {
+        return res.sendStatus(403); // return 403 if token is invalid
+      }
+
+      req.user = user as JwtPayload; // add user to request object
+      return next(); // call next middleware
+  });
+} else {
+    res.sendStatus(401); 
+}
 };
